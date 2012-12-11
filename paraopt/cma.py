24fa86c44882d5f34a46c1a184efbcbb67bc38bf
Toon Verstraelen <Toon.Verstraelen@UGent.be>
2012-12-11 15:35:49 +0100

    More Complete CMA algorithm

--- paraopt/cma.py
+++ paraopt/cma.py
@@ -21,6 +21,7 @@
 
 
 import numpy as np
+from scipy.special import gamma
 import bisect
 
 from paraopt import context
@@ -32,55 +33,82 @@ __all__ = [
 
 
 class CovarianceModel(object):
-    def __init__(self, m0, sigma0):
+    def __init__(self, m0, sigma0, nselect):
+        # Initialize state
         self.m = np.array(m0, dtype=float)
         if len(self.m.shape) != 1:
             raise TypeError('The initial guess must be a vector')
-
-        if isinstance(sigma0, np.ndarray):
-            if len(sigma0.shape) == 0:
-                self.covar = np.identity(self.ndof, float)*sigma0**2
-            elif len(sigma0.shape) == 1:
-                if sigma0.shape[0] != self.ndof:
-                    raise TypeError('The size of sigma0 does not match the size of the initial guess.')
-                self.covar = np.diag(sigma0**2)
-            elif len(sigma0.shape) == 2:
-                if sigma0.shape[0] != self.ndof or sigma0.shape[1] != self.ndof:
-                    raise TypeError('The size of sigma0 does not match the size of the initial guess.')
-                self.covar = 0.5*(sigma0 + sigma0.T)
-            else:
-                raise TypeError('sigma0 must have at most two dimensions.')
-        else:
-            self.covar = np.identity(self.ndof, float)*sigma0**2
-
+        self.ndof = self.m.shape[0]
+        self.sigma = float(sigma0)
+        self.covar = np.identity(self.ndof, dtype=float)
         self._update_derived()
+
+        # Set algorithm parameters
+        self.nselect = nselect
+        self.weights = self.nselect-np.arange(self.nselect, dtype=float)
+        self.weights /= self.weights.sum()
+        self.mu_eff = 1/(self.weights**2).sum()
+
+        # Learning rates for the covariance matrix
+        self.c_1 = 2/self.ndof**2
+        self.c_mu = min(self.mu_eff/self.ndof**2, 1-self.c_1)
+        self.c_old = 1 - self.c_1 - self.c_mu
+
+        # Learning rates for the paths
+        self.c_path_c = 3.0/self.ndof
+        self.root_c = np.sqrt(self.c_path_c*(2-self.c_path_c)*self.mu_eff)
+        self.c_path_sigma = 1.0/self.ndof
+        self.root_sigma = np.sqrt(self.c_path_sigma*(2-self.c_path_sigma)*self.mu_eff)
+        self.ref_path_sigma_norm = np.sqrt(2.0)*gamma(0.5*(self.ndof+1))/gamma(0.5*self.ndof)
+
+        # Initial path arrays
+        self.path_c = np.zeros(self.ndof, float)
+        self.path_sigma = np.zeros(self.ndof, float)
 
     def _update_derived(self):
         # Compute derived properties from the covariance matrix
         self.evals, self.evecs = np.linalg.eigh(self.covar)
         self.sigmas = self.evals**0.5
-        self.max_sigma = abs(self.sigmas).max()
-        self.min_sigma = abs(self.sigmas).min()
-
-    def _get_ndof(self):
-        return self.m.shape[0]
-
-    ndof = property(_get_ndof)
+        self.max_sigma = abs(self.sigmas).max()*self.sigma
+        self.min_sigma = abs(self.sigmas).min()*self.sigma
+        self.inv_root_covar = np.dot(self.evecs/self.sigma, self.evecs.T)
 
     def generate(self, npop):
-        xs = np.random.normal(0, 1, (npop, self.ndof))
+        xs = np.random.normal(0, self.sigma, (npop, self.ndof))
         xs *= self.sigmas
         xs = np.dot(xs, self.evecs)
         xs += self.m
         return xs
 
     def update_covar(self, xs, fs):
-        nselect = len(fs)
-        weights = nselect-np.arange(nselect, dtype=float)
-        weights /= weights.sum()
-        ys = xs - self.m # must be done with old mean!
-        self.covar = np.dot(weights*ys.T, ys)
-        self.m = np.dot(weights, xs)
+        # transform xs to reduced displacements
+        ys = (xs - self.m)/self.sigma # must be done with old mean!
+
+        # compute the new mean
+        new_m = np.dot(self.weights, xs)
+
+        # update the evolution path for the cumulation effect
+        self.path_c = (1-self.c_path_c)*self.path_c + \
+                      self.root_c*(new_m - self.m)/self.sigma
+
+        # update the evolution path for the step size
+        self.path_sigma = (1-self.c_path_sigma)*self.path_sigma + \
+                          self.root_sigma*np.dot(self.inv_root_covar, (new_m - self.m))/self.sigma
+
+        # update the covariance matrix
+        self.covar = sum([
+            self.c_old * self.covar,
+            self.c_1 *   np.outer(self.path_c, self.path_c),
+            self.c_mu *  np.dot(self.weights*ys.T, ys),
+        ])
+
+        # assign the new mean
+        self.m = new_m
+
+        # update the step size
+        path_sigma_norm = np.linalg.norm(self.path_sigma)
+        self.sigma = self.sigma*np.exp(self.c_path_sigma*(path_sigma_norm/self.ref_path_sigma_norm - 1))
+
         self._update_derived()
 
 
@@ -97,8 +125,7 @@ def fmin_cma(fun, m0, sigma0, npop, maxiter=100, cntol=1e6, stol=1e-12, rtol=Non
             The initial guess. (numpy vector, shape=n)
 
        sigma0
-            The initial value of the width of the distribution (a single scalar,
-            a diagonal of the covariance or a covariance matrix)
+            The initial value of the step size
 
        npop
             The size of the sample population
@@ -128,10 +155,10 @@ def fmin_cma(fun, m0, sigma0, npop, maxiter=100, cntol=1e6, stol=1e-12, rtol=Non
     '''
 
     # A) Parse the arguments:
-    cm = CovarianceModel(m0, sigma0)
     if not isinstance(npop, int) or npop < 2:
         raise ValueError('npop must be an integer not smaller than 2.')
     nselect = npop/2
+    cm = CovarianceModel(m0, sigma0, nselect)
 
     # B) The main loop
     if verbose:
